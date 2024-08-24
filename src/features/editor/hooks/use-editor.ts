939839ad32635b32a5fa00dec46bcfb39e579a35
Michael Frieze <michaelfrieze@icloud.com>
2024-08-24 19:22:52 -0400

    feat: hotkeys

--- src/features/editor/hooks/use-editor.ts
+++ src/features/editor/hooks/use-editor.ts
@@ -25,6 +25,7 @@ import { useAutoResize } from "./use-auto-resize";
 import { useCanvasEvents } from "./use-canvas-events";
 import { useClipboard } from "./use-clipboard";
 import { useHistory } from "./use-history";
+import { useHotkeys } from "./use-hotkeys";
 
 const buildEditor = ({
   save,
@@ -557,6 +558,15 @@ export const useEditor = ({
     canvas,
     setSelectedObjects,
     clearSelectionCallback,
+  });
+
+  useHotkeys({
+    undo,
+    redo,
+    copy,
+    paste,
+    save,
+    canvas,
   });
 
   const editor = useMemo(() => {
---
+++ src/features/editor/hooks/use-hotkeys.ts
@@ -0,0 +1,74 @@
+import { fabric } from "fabric";
+import { useEvent } from "react-use";
+
+interface UseHotkeysProps {
+  canvas: fabric.Canvas | null;
+  undo: () => void;
+  redo: () => void;
+  save: (skip?: boolean) => void;
+  copy: () => void;
+  paste: () => void;
+}
+
+export const useHotkeys = ({
+  canvas,
+  undo,
+  redo,
+  save,
+  copy,
+  paste,
+}: UseHotkeysProps) => {
+  useEvent("keydown", (event) => {
+    const isCtrlKey = event.ctrlKey || event.metaKey;
+    const isBackspace = event.key === "Backspace";
+    const isInput = ["INPUT", "TEXTAREA"].includes(
+      (event.target as HTMLElement).tagName,
+    );
+
+    if (isInput) return;
+
+    if (isBackspace) {
+      canvas?.remove(...canvas.getActiveObjects());
+      canvas?.discardActiveObject();
+    }
+
+    if (isCtrlKey && event.key === "z") {
+      event.preventDefault();
+      undo();
+    }
+
+    if (isCtrlKey && event.key === "y") {
+      event.preventDefault();
+      redo();
+    }
+
+    if (isCtrlKey && event.key === "c") {
+      event.preventDefault();
+      copy();
+    }
+
+    if (isCtrlKey && event.key === "v") {
+      event.preventDefault();
+      paste();
+    }
+
+    if (isCtrlKey && event.key === "s") {
+      event.preventDefault();
+      save(true);
+    }
+
+    if (isCtrlKey && event.key === "a") {
+      event.preventDefault();
+      canvas?.discardActiveObject();
+
+      const allObjects = canvas
+        ?.getObjects()
+        .filter((object) => object.selectable);
+
+      canvas?.setActiveObject(
+        new fabric.ActiveSelection(allObjects, { canvas }),
+      );
+      canvas?.renderAll();
+    }
+  });
+};
