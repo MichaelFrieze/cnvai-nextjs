880d565fc72302555ddc6abbc448038a58ddb539
Michael Frieze <michaelfrieze@icloud.com>
2024-08-26 21:43:46 -0400

    feat: setup authjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div>
-      <Button>Click me</Button>
+      <h1>{JSON.stringify(session)}</h1>
     </div>
   );
 }
