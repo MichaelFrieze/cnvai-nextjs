cbe176b9c3850ed2cb018deb61acbc59721cd9b4
Michael Frieze <michaelfrieze@icloud.com>
2024-08-31 15:25:33 -0400

    feat: db and drizzle setup

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+
+import { db } from "@/db/drizzle";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [GitHub],
 });
