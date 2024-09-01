99dfa976871964bb11bdf09e80d20fa70f1266e9
Michael Frieze <michaelfrieze@icloud.com>
2024-09-01 15:18:42 -0400

    feat: google login

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,13 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 import { db } from "@/db/drizzle";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [GitHub],
+  providers: [GitHub, Google],
   pages: {
     signIn: "/sign-in",
     error: "/sign-in",
